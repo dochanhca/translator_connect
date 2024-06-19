aff36a630b96ceb6f45be67a9fd842943f8ac78c
duc2pham <duc2.pham@lge.com>
2024-06-19 22:45:31 +0700

    init commit

---
+++ app/src/main/java/com/example/translateconnector/data/network/ConnectivityInterceptor.java
@@ -0,0 +1,34 @@
+package com.example.translateconnector.data.network;
+
+import android.content.Context;
+
+import com.imoktranslator.exception.NoConnectivityException;
+import com.imoktranslator.utils.NetworkUtils;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by ton on 4/5/18.
+ */
+
+public class ConnectivityInterceptor implements Interceptor {
+    private Context mContext;
+
+    public ConnectivityInterceptor(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        if (!NetworkUtils.isOnline(mContext)) {
+            throw new NoConnectivityException();
+        }
+
+        Request.Builder builder = chain.request().newBuilder();
+        return chain.proceed(builder.build());
+    }
+}
